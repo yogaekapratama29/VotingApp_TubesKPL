09871dc4adce159152a999e7a4b1608ae92f948c
DavidCatch <raihanwibyanto@gmail.com>
2025-04-28 10:36:48 +0700

    +feat: delete vote

--- public/app.js
+++ public/app.js
@@ -20,27 +20,3 @@ async function createPolling() {
 
   loadPollings();
 }
-
-/* 3. Fungsi untuk melakukan voting*/
-async function vote(contributor) {
-  //Mendapatkan nilai dari input form:
-  // 1. Nama voter dari elemen dengan ID 'voter-{contributor}'
-  const voterName = document.getElementById(`voter-${contributor}`).value;
-  // 2. Pilihan vote dari dropdown/select dengan ID 'choice-{contributor}'
-  const choice = document.getElementById(`choice-${contributor}`).value;
-
-  // Mengirimkan data vote ke server menggunakan fetch API
-  await fetch(`/pollings/${contributor}/vote`, {
-    method: 'POST', // Metode HTTP POST
-    headers: {
-      'Content-Type': 'application/json', // Spesifikasikan tipe konten JSON
-    },
-    // Mengubah objek JavaScript ke format JSON
-    body: JSON.stringify({
-      voterName, // Nama voter yang mengisi form
-      choice, //Pilihan yang dipilih
-    }),
-  });
-  // Menampilkan notifikasi bahwa vote berhasil
-  alert('Vote berhasil!');
-}
